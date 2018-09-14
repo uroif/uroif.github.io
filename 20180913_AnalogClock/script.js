b9deb17b7362b3ba99e688b553f7bac03b2b276c
uroif <fiorume@gmail.com>
2018-09-14 08:30:48 +0700

    Clock

--- 20180913_AnalogClock/script.js
+++ 20180913_AnalogClock/script.js
@@ -1,33 +1,36 @@
-function clock(){
-    //calculate angle
-    var d, h, m, s;
-    d = new Date;
-    
-    h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
-    m = 6 * d.getMinutes();
-    s = 6 * d.getSeconds();
-    
-    //move hands
-    setAttr('h_hand', h);
-    setAttr('m_hand', m);
-    setAttr('s_hand', s);
-    setAttr('s_tail', s+180);
-    
-    //call every second
-    setTimeout(clock, 1000);
-    
-};
-
-function setAttr(id, val) {
-    var v = 'rotate(' + val + ', 300, 300)';
-    document.getElementById(id).setAttribute('transform', v);
-};
-
-function setText(id, val) {
-    if(val < 10) {
-        val = '0' + val;
+setInterval(function() {
+    function r(el, deg) {
+        el.setAttribute('transform', 'rotate('+ deg +' 50 50)')
     }
-    document.getElementById(id).innerHTML = val;
-};
+    var d = new Date()
+    r(second, 6*d.getSeconds())  
+    r(min, 6*d.getMinutes())
+    r(hour, 30*(d.getHours()%12) + d.getMinutes()/2)
+}, 1000)
 
-window.onload=clock;
+// function clock(){
+//     //calculate angle
+//     var d, h, m, s;
+//     d = new Date;
+    
+//     h = 30 * ((d.getHours() % 12) + d.getMinutes() / 60);
+//     m = 6 * d.getMinutes();
+//     s = 6 * d.getSeconds();
+    
+//     //move hands
+//     setAttr('hour', h);
+//     setAttr('min', m);
+//     setAttr('second', s);
+//     setAttr('s_sec', s+180);
+    
+//     //call every seconds
+//     setTimeout(clock, 1000);
+    
+// };
+
+// function setAttr(cls, val) {
+//     var v = 'rotate(' + val + ', 300, 300)';
+//     document.getElementsByClassName(cls).setAttribute('transform', v);
+// };
+
+// window.onload=clock;
