05e6f02bc021106b2644c9ae9d07d8131e6dd250
Web2 <web2@TechMasters-iMac-4.local>
2018-09-28 09:14:23 +0700

    Vue

--- node_modules/vue-konva/src/utils/updatePicture.js
+++
@@ -1,6 +0,0 @@
-// adapted FROM: https://github.com/lavrton/react-konva/blob/master/src/react-konva-fiber.js
-
-export default function updatePicture(node) {
-  var drawingNode = node.getLayer() || node.getStage();
-  drawingNode && drawingNode.batchDraw();
-}
