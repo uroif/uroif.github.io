069a1120063876700f77ff76d59550c36c3f247d
uroif <fiorume@gmail.com>
2018-09-29 11:09:54 +0700

    Deploy

--- shopping_cart/deploy.sh
+++ shopping_cart/deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:uroif/uroif.github.io.git master
+# git push -f git@github.com:uroif/uroif.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:uroif/shopping_cart.git master:gh-pages
 
 cd -
