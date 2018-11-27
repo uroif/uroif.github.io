4943eb178d7cae48f82228394970dc2f83745257
uroif <fiorume@gmail.com>
2018-11-27 22:34:33 +0700

    responsive web

--- 20181008_ResponsiveWeb/script.js
+++ 20181008_ResponsiveWeb/script.js
@@ -17,55 +17,62 @@ $(".dropdown").on("hide.bs.dropdown", function() {
 });
 
 // Add slideshow for items last section
-$(".wrap-slider").slick({
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 5000,
-  dots: false,
-  pauseOnFocus: true,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        infinite: true
+$(document).ready(function() {
+  $(".wrap-slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    dots: false,
+    pauseOnFocus: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 300,
+        settings: {
+          slidesToShow: 1,
+          dots: true
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 300,
-      settings: {
-        slidesToShow: 1,
-        dots: true
-      }    
-    }
-  ]
+    ]
+  });
 });
 
 // Slider for modal
-$('.modal-slider-for').slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-  fade: true,
-  autoplay: true,
-  asNavFor: '.modal-slider-nav',
-});
-$('.modal-slider-nav').slick({
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  asNavFor: '.modal-slider-for',
-  dots: false,
-  centerMode: true,
-  autoplay: true,
-  focusOnSelect: true
+$(".modal").ready(function() {
+  $(".modal-slider-for").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: ".modal-slider-nav",
+    arrows: false,
+    autoplay: true,
+    centerMode: true,
+    waitForAnimate: true,
+    fade: true
+  });
+  $(".modal-slider-nav").slick({
+    slidesToShow: 3,
+    asNavFor: ".modal-slider-for",
+    dots: false,
+    arrows: false,
+    vertical: true,
+    focusOnSelect: true,
+    autoplay: true,
+    settings: "unslick",
+  });
 });
 
 // Keep navigation when scroll down
