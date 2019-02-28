fb067e820e165e65e85eccab413e7c5e635ab966
uroif <fiorume@gmail.com>
2019-02-28 18:07:08 +0700

    update

--- cywatch/script.js
+++ cywatch/script.js
@@ -1,27 +1,61 @@
-// SlideDown animation to Bootstrap dropdown when expanding
-// $(".dropdown").on("show.bs.dropdown", function() {
-//   $(this)
-//     .find(".dropdown-menu")
-//     .first()
-//     .stop(true, true)
-//     .slideDown(300);
-// });
+if (screen.width >= 768) {
+  $('ul.navbar-nav li.dropdown').hover(function () {
+    $(this).find('.dropdown-menu-wrap').stop(true, true).delay(50).slideDown(200);
+  }, function () {
+    $(this).find('.dropdown-menu-wrap').stop(true, true).delay(50).slideUp(200);
+  });
+}
 
-// SlideUp animation to Bootstrap dropdown when collapsing
-// $(".dropdown").on("hide.bs.dropdown", function() {
-//   $(this)
-//     .find(".dropdown-menu")
-//     .first()
-//     .stop(true, true)
-//     .slideUp(300);
-// });
+if (screen.width < 768) {
+  // SlideDown animation to Bootstrap dropdown when expanding
+  $(".dropdown").on("show.bs.dropdown", function () {
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .slideDown(200);
+  });
+  // SlideUp animation to Bootstrap dropdown when collapsing
+  $(".dropdown").on("hide.bs.dropdown", function () {
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .slideUp(200);
+  });
+}
 
-// Hover item to Bootstrap dropdown
-$('ul.navbar-nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(300);
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(300);
-});
+// function myFunction(x) {
+//   if (x.matches) {
+//     // Hover item to Bootstrap dropdown
+//     $('ul.navbar-nav li.dropdown').hover(function () {
+//       $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeIn(200);
+//     }, function () {
+//       $(this).find('.dropdown-menu').stop(true, true).delay(50).fadeOut(200);
+//     });
+//   } else {
+//     // SlideDown animation to Bootstrap dropdown when expanding
+//     $(".dropdown").on("show.bs.dropdown", function () {
+//       $(this)
+//         .find(".dropdown-menu")
+//         .first()
+//         .stop(true, true)
+//         .slideDown(300);
+//     });
+//     // SlideUp animation to Bootstrap dropdown when collapsing
+//     $(".dropdown").on("hide.bs.dropdown", function () {
+//       $(this)
+//         .find(".dropdown-menu")
+//         .first()
+//         .stop(true, true)
+//         .slideUp(300);
+//     });
+//   }
+// }
+
+// var x = window.matchMedia("(min-width: 768px)")
+// myFunction(x)
+// x.addListener(myFunction)
 
 // Slideshow for promotions
 $(".promotion").slick({
